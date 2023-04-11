e1271c10ea73e4aba85073f4e7adeea52a3a54a6
Charanvir <charanvir123@gmail.com>
2023-04-11 15:59:48 -0400

    Replaced missing data with mean of column, encoded the string values, hot encoding for country category, simple label encoding for the purchasing column

--- data_preprocessing_tools.py
+++ data_preprocessing_tools.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 
 dataset = pd.read_csv("Data.csv")
 # Features
@@ -8,6 +12,16 @@ x = dataset.iloc[:, :-1].values
 # Dependent variable vector
 y = dataset.iloc[:, -1:].values
 
-print(dataset)
-print(x)
+# Handling the missing data
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+# Encoding the Independent Variable
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder="passthrough")
+x = np.array(ct.fit_transform(x))
+
+# Encoding the Dependent Variable
+le = LabelEncoder()
+y = le.fit_transform(y)
 print(y)
