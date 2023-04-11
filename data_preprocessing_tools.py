b43f0fa7a92ddc64b938ef4915146132ca6666ea
Charanvir <charanvir123@gmail.com>
2023-04-11 16:26:23 -0400

    split dataset into training and testing set

--- data_preprocessing_tools.py
+++ data_preprocessing_tools.py
@@ -5,6 +5,7 @@ from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 
 dataset = pd.read_csv("Data.csv")
 # Features
@@ -24,4 +25,6 @@ x = np.array(ct.fit_transform(x))
 # Encoding the Dependent Variable
 le = LabelEncoder()
 y = le.fit_transform(y)
-print(y)
+
+# Splitting the dataset
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
